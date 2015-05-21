8d844efec394f7ebc113454852813029ce52775b
bill-brikiatis <brikiatis@g.harvard.edu>
2015-05-21 16:03:02 -0400

    Complete some of mobile menu

---
+++ js/altos.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+	$("#mobileDropdown").click(function(){
+		$("#logo").css("display", "none");
+		$("#bag").css("display", "none");
+		$("#mobileDropdown").css("display", "none");
+	});
+});
+
+
